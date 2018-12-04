3e726272b53ee76c8badef5ef94bde21a726da92
alec27 <wieseal@pdx.edu>
2018-12-04 15:22:23 -0800

    Completed hardware with motor control

--- android_things_app/HWProject/app/src/main/java/alec/androidthingsece558/HomeActivity.java
+++ android_things_app/HWProject/app/src/main/java/alec/androidthingsece558/HomeActivity.java
@@ -30,7 +30,6 @@ public class HomeActivity extends Activity {
 
     //Delay time for runnables
     private static final int INTERVAL_BETWEEN_BLINKS_MS = 1000;
-    private static final int INTERVAL_BETWEEN_UPDATES_MS = 5000;
 
     // I2C Slave Address
     private static final int I2C_ADDRESS = 0x08;
@@ -96,7 +95,7 @@ public class HomeActivity extends Activity {
 
             // Step 4. Repeat using a handler.
             mHandler.post(mBlinkRunnable);
-            mHandler.post(mUpdateRunnable);
+            //mHandler.post(mUpdateRunnable);
         } catch (IOException e) {
             Log.e(TAG, "Error on PeripheralIO API", e);
         }
@@ -141,10 +140,15 @@ public class HomeActivity extends Activity {
                     Object bluePWM = dataSnapshot.child("iotDevice1").child(PWM_BLUE_LED).getValue();
                     byte bluePWMbyte = getPWMByte(bluePWM);
 
-                    //Write the LED PWM values to their respective registers
+                    //Get the Motor PWM byte to send
+                    Object motorPWM = dataSnapshot.child("iotDevice1").child(PWM_MOTOR).getValue();
+                    byte motorPWMbyte = getPWMByte(motorPWM);
+
+                    //Write the LED and motor PWM values to their respective registers
                     writeRegister(mDevice, addressLEDRed, redPWMbyte);
                     writeRegister(mDevice, addressLEDGreen, greenPWMbyte);
                     writeRegister(mDevice, addressLEDBlue, bluePWMbyte);
+                    writeRegister(mDevice, addressMotor, motorPWMbyte);
 
                     //Get the DAC output value that we want to set (5 bits)
                     Object DAC1OUT = dataSnapshot.child("iotDevice1").child("DAC1OUT").getValue();
@@ -205,110 +209,6 @@ public class HomeActivity extends Activity {
         }
     };
 
-    //Create a runnable that updates every 5 seconds to send updated data to the database
-    private Runnable mUpdateRunnable = new Runnable() {
-        @Override
-        public void run() {
-            //Calculate the temperature
-            //10mV per degree celcius
-            //Voltage output max is 2000mV according to the datasheet
-            //750mV output at 25C
-
-            //Determine the base case ratio for 25C
-            double celcius25Ratio = 0.750/2.0;
-
-            //Divide digital reading by maximum of 2047 (10 bits all set to '1')
-            int digitalTemp = digitalToInt(ADA5_ADDRESS_MSB, ADA5_ADDRESS_LSB);
-            double digitalTempRatio = digitalTemp/2047.0;
-
-            //Get the voltage difference between this reading and 25C
-            double voltageDiff = (digitalTempRatio - celcius25Ratio) * 2.0;
-
-            //Use the 10mV/C  ratio to get temperature and add the offset of 25C
-            double temperature = (voltageDiff * (1.0/0.1)) + 25.0;
-
-            //Also provide a farenheit reading
-            double temperatureFarenheit = (temperature * 9.0)/5.0 + 32.0;
-
-            //Log the temperature for debug (Easier for me to validate in degrees F)
-            Log.d(TAG, "Temperature is " + temperatureFarenheit);
-
-            //Update the database with the digital reading and the C and F temperatures
-            mDatabase.child("iotDevice1").child("ADA5IN").setValue(digitalTemp);
-            mDatabase.child("iotDevice1").child("TEMPERATURE").setValue(temperature);
-            mDatabase.child("iotDevice1").child("TEMPERATURE_F").setValue(temperatureFarenheit);
-
-            //Determine the motor PWM (out of 100) based on the temperature in C
-            int motorPWMval;
-            if ( (temperature > 15) & (temperature<= 18) ) {
-                motorPWMval = 30;
-            }
-            else if ( (temperature > 18) & (temperature<= 22) ) {
-                motorPWMval = 50;
-            }
-            else if ( (temperature > 22) & (temperature<= 25) ) {
-                motorPWMval = 70;
-            }
-            else if (temperature< 15) {
-                motorPWMval = 80;
-            }
-            else {
-                motorPWMval = 40;
-            }
-
-            //Convert int to byte and write to the PWM address ofmotor register
-            byte motorSetVal = getPWMbyteFromInt(motorPWMval);
-            writeRegister(mDevice, addressMotor, motorSetVal);
-
-            //Update the database with the motor PWM
-            mDatabase.child("iotDevice1").child("PWM_MOTOR").setValue(motorPWMval);
-
-            //Update the database with the ADC values for ADC3,4,5 in integer format
-            int ADC3 = digitalToInt(ADC3_ADDRESS_MSB, ADC3_ADDRESS_LSB);
-            mDatabase.child("iotDevice1").child("ADC3IN").setValue(ADC3);
-            int ADC4 = digitalToInt(ADC4_ADDRESS_MSB, ADC4_ADDRESS_LSB);
-            mDatabase.child("iotDevice1").child("ADC4IN").setValue(ADC4);
-            int ADC5 = digitalToInt(ADC5_ADDRESS_MSB, ADC5_ADDRESS_LSB);
-            mDatabase.child("iotDevice1").child("ADC5IN").setValue(ADC5);
-
-            //Schedule an update every 5 seconds
-            mHandler.postDelayed(mUpdateRunnable, INTERVAL_BETWEEN_UPDATES_MS);
-
-            //Update the timestamp with local time
-            //Uses code from https://stackoverflow.com/questions/23068676/how-to-get-current-timestamp-in-string-format-in-java-yyyy-mm-dd-hh-mm-ss
-            // and https://stackoverflow.com/questions/1305350/how-to-get-the-current-date-and-time-of-your-timezone-in-java
-            Date date = new Date();
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            df.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
-            String timeStamp = "Pacific time: " + df.format(date);//new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(Calendar.getInstance().getTime());
-            mDatabase.child("iotDevice1").child("TIMESTAMP").setValue(timeStamp);
-
-        }
-    };
-
-    //Convert register value to an integer
-
-    /**
-     * Converts two bytes (10 bits) into an integer
-     * @param addressMSB Is the higher byte
-     * @param addressLSB Is the lower byte
-     * @return The converted integer value
-     */
-    public int digitalToInt(byte addressMSB, byte addressLSB) {
-        byte[] adcVal = new byte[2];
-        adcVal[0] = 0x03;//set two LSB bits to 1
-        //Set the upper byte to the lower two bits of the MSB address byte
-        adcVal[0] &= readRegister(mDevice, addressMSB);
-        //Set the lower byte to the LSB address byte
-        adcVal[1] = readRegister(mDevice, addressLSB);
-
-        //From https://stackoverflow.com/questions/8091022/how-do-you-append-two-bytes-to-an-int
-        //Concatenate the two bytes and cast to integer
-        int adcDecimal = ((adcVal[0] & 0xFF) << 8) | (adcVal[1] & 0xFF) ;
-
-        return  adcDecimal;
-    }
-
     /**
      * Converts encapsulated string object into a byte.
      * Does not handle overflow
@@ -365,33 +265,6 @@ public class HomeActivity extends Activity {
 
         return PWMbyte;
     }
-
-    /**
-     * Convert an integer to a byte to be written to a register
-     * @param inputPWMInt Integer PWM value (out of 100)
-     * @return The PWM in byte format
-     */
-    public byte getPWMbyteFromInt(int inputPWMInt) {
-        //Define maximum and minimum values
-        if (inputPWMInt > 100) {
-            inputPWMInt = 100;
-        }
-        else if (inputPWMInt < 0) {
-            inputPWMInt = 0;
-        }
-
-        //Then to a hexadecimal string
-        String HexString = Integer.toHexString(inputPWMInt);
-
-        //Convert the hex value to a byte
-        byte[] PWMHex = hexStringToByteArray(HexString);
-
-        byte PWMbyte = PWMHex[0];
-
-        return PWMbyte;
-    }
-
-
 
     /**
      * Write byte value to a register via I2C
@@ -453,7 +326,7 @@ public class HomeActivity extends Activity {
         //GPIO/LED blink code from https://developer.android.com/things/training/first-device/peripherals
         // Step 4. Remove handler events on close.
         mHandler.removeCallbacks(mBlinkRunnable);
-        mHandler.removeCallbacks(mUpdateRunnable);
+        //mHandler.removeCallbacks(mUpdateRunnable);
 
         // Step 5. Close the resource.
         if (mLedGpio != null) {
